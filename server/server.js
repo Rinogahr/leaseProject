fdb9eeb78bf87b9525a095c43573864b76959fe7
Rodrigo Maciel <69148290+Rinogahr@users.noreply.github.com>
2022-09-19 21:15:36 -0300

    zx
    xzxzx

--- server/server.js
+++ server/server.js
@@ -6,6 +6,9 @@ app.use(express.json());
 
 // http://localhost:3000/location
 // CEP PARA TESTE 51330110 / 51240000 / 51330098
+let endereco1 = [];
+let endereco2 = "";
+let endereco3 = "";
 
 app.get("/", ( rec, res ) =>{
     const cep1 = rec.query.cep1;
@@ -18,9 +21,20 @@ app.get("/", ( rec, res ) =>{
 });
 app.get("/location/:cep1/:cep2/:cep3", ( rec, res ) =>{
     const  {cep1,cep2,cep3}  = rec.params;
+
+    endereco1 = fetch(`https://nominatim.openstreetmap.org/search.php?postalcode=${cep1}&format=jsonv2`)
+    .then( function (data) {
+        console.log(data);
+    }).catch( function(error){
+        console.log(error)
+    })
+    endereco2 = `https://nominatim.openstreetmap.org/search.php?postalcode=${cep2}&format=jsonv2`;
+    endereco3 = `https://nominatim.openstreetmap.org/search.php?postalcode=${cep3}&format=jsonv2`;
+
+
+    //https://viacep.com.br/ws/51330590/json/
     
-     res.json( { "cep1":cep1, "cep2":cep2, "cep3":cep3 } );
-     console.log('ola mundo')
+     res.json( { "endereco1":endereco1, "endereco2":endereco2, "endereco3":endereco3 } );
     
 });
 
