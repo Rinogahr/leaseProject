913da69c35c50fc09552ef9b0831c3de33461951
Rinogahr <69148290+Rinogahr@users.noreply.github.com>
2022-09-21 18:08:50 -0300

    dsds
    sdsd

--- frontend/src/pages/map/MapPage.jsx
+++ frontend/src/pages/map/MapPage.jsx
@@ -15,53 +15,51 @@ function MapPage() {
     googleMapsApiKey: "AIzaSyB5s0fRbWRykQG2ud_ygzzZEXGaxY1rpns"
   });
 
-    return (
-      <div className={myStyle.containerMap}>
-        <div className={myStyle.mapCombobox}>
-          <div className={myStyle.campos}>
-            <div>
-              {/* <InputSplitRpm 
-              txtLabel="Infome o primeiro endereço"/> */}
-              <input
-                type="text"
-                id="username"
-                // className={styleimput.textInput}
-                autoComplete="off"
-                placeholder="Informe o primeiro CEP"
-                // onChange={onChange}
-                value="dsdsd"
-              />
-            </div>
-            <div>
-              {/* <InputSplitRpm 
-              txtLabel="Infome o primeiro endereço"/> */}
-              <input
-                type="text"
-                id="username"
-                // className={styleimput.textInput}
-                autoComplete="off"
-                placeholder="Informe o segundo CEP"
-                // onChange={onChange}
-                value="dsdsd"
-              />
-            </div>
-            <div>
-              {/* <InputSplitRpm 
-              txtLabel="Infome o primeiro endereço"/> */}
-              <input
-                type="text"
-                id="username"
-                // className={styleimput.textInput}
-                autoComplete="off"
-                placeholder="Informe o terceiro CEP"
-                // onChange={onChange}
-                value="dsdsd"
-              />
+
+    return(
+      <div className={myStyle.container}>
+
+        <div className={myStyle.childrenLeft}>
+          <div className={myStyle.childrenLeftContainerUm}>
+            <div className={myStyle.childrenLeftContainerUmchildren}>
+              <div className={myStyle.childrenLeftTitle}>
+                <b>Projeto de Geolocalização RPM</b>
+              </div>
+              <div className={myStyle.childrenLeftInputs}>
+
+                <div className={myStyle.inputContainer}>
+                  <input type="number" name="cep1" id="cep1" className={myStyle.inputCep}  placeholder="exemplo: 11111111"/>
+                  <label for="{nameFor}" className={myStyle.label}>Informe o primeiro CEP</label>
+                </div>
+
+                <div className={myStyle.inputContainer}>
+                  <input type="number" name="cep1" id="cep2" className={myStyle.inputCep}  placeholder="exemplo: 22222222"/>
+                  <label for="{nameFor}" className={myStyle.label}>Informe o segundo CEP</label>
+                </div>
+
+                <div className={myStyle.inputContainer}>
+                  <input type="number" name="cep1" id="cep3" className={myStyle.inputCep}  placeholder="exemplo: 33333333"/>
+                  <label for="{nameFor}" className={myStyle.label}>Informe o terceiro CEP</label>
+                </div>
+                 
+              </div>
+              <div className={myStyle.childrenLeftButton}>
+                <div className={myStyle.textBox}>
+                    <a href="#" className={myStyle.btn} class="btn btn-white btn-animate">Consultar</a>
+                </div>
+              </div>
             </div>
           </div>
+
+          <div className={myStyle.childrenLeftContainerDois}>
+            <div className={myStyle.childrenLeftTitleDois}>1</div>
+            <div className={myStyle.childrenLeftInputsDois}>2</div>
+            <div className={myStyle.childrenLeftButtonDois}>3</div>
+          </div>
         </div>
-        <div className={myStyle.googleMapCt}>
-          {
+
+        <div className={myStyle.childrenRigth }>
+        {
             isLoaded ? (
               <GoogleMap
                 mapContainerStyle={{width: '100%', height: '100%'}}
@@ -83,6 +81,57 @@ function MapPage() {
         </div>
       </div>
     );
+
+    // return (
+    //   <div className={myStyle.containerMap}>
+    //     <div className={myStyle.mapCombobox}>
+    //       <div className={myStyle.campos}>
+    //         <div>
+    //           {/* <InputSplitRpm 
+    //           txtLabel="Infome o primeiro endereço"/> */}
+    //           <input
+    //             type="text"
+    //             id="username"
+    //             // className={styleimput.textInput}
+    //             autoComplete="off"
+    //             placeholder="Informe o primeiro CEP"
+    //             // onChange={onChange}
+    //             value="dsdsd"
+    //           />
+    //         </div>
+    //         <div>
+    //           {/* <InputSplitRpm 
+    //           txtLabel="Infome o primeiro endereço"/> */}
+    //           <input
+    //             type="text"
+    //             id="username"
+    //             // className={styleimput.textInput}
+    //             autoComplete="off"
+    //             placeholder="Informe o segundo CEP"
+    //             // onChange={onChange}
+    //             value="dsdsd"
+    //           />
+    //         </div>
+    //         <div>
+    //           {/* <InputSplitRpm 
+    //           txtLabel="Infome o primeiro endereço"/> */}
+    //           <input
+    //             type="text"
+    //             id="username"
+    //             // className={styleimput.textInput}
+    //             autoComplete="off"
+    //             placeholder="Informe o terceiro CEP"
+    //             // onChange={onChange}
+    //             value="dsdsd"
+    //           />
+    //         </div>
+    //       </div>
+    //     </div>
+    //     <div className={myStyle.googleMapCt}>
+    //       
+    //     </div>
+    //   </div>
+    // );
   }
   
 
--- server/server.js
+++ server/server.js
@@ -1,14 +1,18 @@
 const express = require('express');
+const axios = require('axios');
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(express.json());
 
 // http://localhost:3000/location
-// CEP PARA TESTE 51330110 / 51240000 / 51330098
+// CEP PARA TESTE 51330110 / 51240000 / 51330098 / 50040000
 let endereco1 = [];
-let endereco2 = "";
-let endereco3 = "";
+let endereco2 = [];
+let endereco3 = [];
+
+
+
 
 app.get("/", ( rec, res ) =>{
     const cep1 = rec.query.cep1;
@@ -20,23 +24,31 @@ app.get("/", ( rec, res ) =>{
     res.json( { "cep3": cep3 } );
 });
 app.get("/location/:cep1/:cep2/:cep3", ( rec, res ) =>{
-    const  {cep1,cep2,cep3}  = rec.params;
+    const  {cep1,cep2,cep3}  =  rec.params;
+    // //https://viacep.com.br/ws/51330590/json/
+    //  res.json( { "endereco1":endereco1, "endereco2":endereco2, "endereco3":endereco3 } );
+    axios.get(`https://nominatim.openstreetmap.org/search.php?postalcode=${cep1}&format=jsonv2`).then(resp => {
 
-    endereco1 = fetch(`https://nominatim.openstreetmap.org/search.php?postalcode=${cep1}&format=jsonv2`)
-    .then( function (data) {
-        console.log(data);
-    }).catch( function(error){
-        console.log(error)
-    })
-    endereco2 = `https://nominatim.openstreetmap.org/search.php?postalcode=${cep2}&format=jsonv2`;
-    endereco3 = `https://nominatim.openstreetmap.org/search.php?postalcode=${cep3}&format=jsonv2`;
-
-
-    //https://viacep.com.br/ws/51330590/json/
+        endereco1 = resp.data;
+    });
     
-     res.json( { "endereco1":endereco1, "endereco2":endereco2, "endereco3":endereco3 } );
+    res.json({"teste1":endereco1});
     
 });
+
+async function doGetRequest(rec1,rec2,rec3) {
+
+    console.log(rec1,rec2,rec3);
+     endereco1 = await axios.get(`https://nominatim.openstreetmap.org/search.php?postalcode=${rec1}&format=jsonv2`);
+     endereco2 = await axios.get(`https://nominatim.openstreetmap.org/search.php?postalcode=${rec2}&format=jsonv2`);
+     endereco3 = await axios.get(`https://nominatim.openstreetmap.org/search.php?postalcode=${rec3}&format=jsonv2`);
+  
+    
+    return endereco1;
+  }
+
+
+
 
 
 
